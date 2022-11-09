0ba19e37dc404196e4dba461b9c702952cb274b1
bellenuit <matti@belle-nuit.com>
2022-11-09 18:54:59 +0100

    3.8.6d
    It’s d version

--- api.php
+++ api.php
@@ -26,7 +26,7 @@
 define('SOFAWIKI',true);  // all included files will check for this variable
 $swError = "";
 $swDebug = "";
-$swVersion = '3.8.6d';  
+$swVersion = '3.8.6d';   
 $swMainName = 'Main';
 $swStartTime = microtime(true);
 $swSimpleURL = false;
