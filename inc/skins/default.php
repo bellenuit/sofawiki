88670a67e48214d6974cb8a8b47616f2adf6b769
matti <matti@belle-nuit.com>
2025-07-23 19:58:44 +0200

    Hot fix
    closing div missed in inc/skin/default.php prevented install script to show the content

--- inc/skins/default.php
+++ inc/skins/default.php
@@ -1,14 +1,10 @@
 <?php 
-
 $skinstylesheet = ''; // default.css is always loaded. add the specific stylesheet	
 include 'header.php';
-
 echo PHP_EOL.'<div id="header">';
 echo PHP_EOL.swSystemMessage("skin-header",$lang, true);
 echo PHP_EOL.'</div><!-- header -->';
-
 echo PHP_EOL.'<div id="menu">';
-
 
 echo PHP_EOL.$swHomeMenu. '<br/>'; 
 foreach($swLangMenus as $item) {echo PHP_EOL.$item.'<br/>'; } 
@@ -22,36 +18,40 @@ if (!count($swEditMenus))
 {
 	echo PHP_EOL.'<div id="mobileappmenu">';
 	echo PHP_EOL.'<div id="mobilemenu" class="dropdownmobilemenu">';
-	echo PHP_EOL.$barHeaderHeader.'≡'.$barHeaderFooter;
-	echo PHP_EOL.$groupHeader;
-	echo PHP_EOL.$itemHeader.$swHomeMenu.$itemFooter; 
-	foreach($swLangMenus as $item) {echo PHP_EOL.$itemHeader.$item.$itemFooter;} 
+	if(isset($barHeaderHeader)) echo PHP_EOL.$barHeaderHeader.'≡'.$barHeaderFooter;
+	if(isset($groupHeader)) echo PHP_EOL.$groupHeader;
+	if(isset($temHeader)) echo PHP_EOL.$itemHeader.$swHomeMenu.$itemFooter; 
+	foreach($swLangMenus as $item) 
+	{
+		 if(isset($temHeader) && isset($itemFooter)) echo PHP_EOL.$itemHeader.$item.$itemFooter;	    
+	} 
 	$sms = swSystemMessage("skin-menu",$lang, true);
 	$sms = str_replace('<p>','',str_replace('<br>','',$sms));
 	$sm = explode(PHP_EOL,$sms);
-	foreach($sm as $item) {echo PHP_EOL.$itemHeader.$item.$itemFooter;} 
+	foreach($sm as $item)
+	{
+		if(isset($temHeader) && isset($itemFooter)) echo PHP_EOL.$itemHeader.$item.$itemFooter;
+	} 
 	foreach($swLoginMenus as $item) 
 	{
 		if ($item == $username)
 		{
-			echo PHP_EOL.$itemSilentHeader.$item.$itemFooter;
+			if(isset($itemSilentHeader) && isset($itemFooter))  echo PHP_EOL.$itemSilentHeader.$item.$itemFooter;
 		}
 		else
 		{
-			echo PHP_EOL.$itemHeader.$item.$itemFooter;
+			if(isset($temHeader) && isset($itemFooter))  echo PHP_EOL.$itemHeader.$item.$itemFooter;
 		}
 	}
-	echo PHP_EOL.$groupFooter;
-	echo PHP_EOL.$barFooter;
-	echo PHP_EOL.$barHeader.$swSingleSearchMenu.$barFooter;
+	if(isset($groupFooter)) echo PHP_EOL.$groupFooter;
+	if(isset($barFooter)) echo PHP_EOL.$barFooter;
+	if(isset($barHeader) && isset($swSingleSearchMenu) && isset($barFooter)) echo PHP_EOL.$barHeader.$swSingleSearchMenu.$barFooter;
+	echo PHP_EOL.'</div><!-- mobilemenu -->';
 	echo PHP_EOL.'</div><!-- mobileappmenu -->';
 }
-
 echo PHP_EOL.'<div id="content">';
 echo PHP_EOL.'<div id="title">';
 echo PHP_EOL.'<h1>'.$swParsedName.'</h1>';
-echo PHP_EOL.'</div><!-- title -->';
-
 echo PHP_EOL.'<div id="parsedcontent">';
 echo $swParsedContent;
 echo PHP_EOL.'</div><!-- parsedcontent -->';
